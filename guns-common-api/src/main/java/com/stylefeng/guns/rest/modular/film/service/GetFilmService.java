ab5b7922ae07e86c0342a218d00a8b05143fecdd
liwenjing1995 <443874266@qq.com>
2019-07-17 23:19:01 +0800

    liwenjing

---
+++ guns-common-api/src/main/java/com/stylefeng/guns/rest/modular/film/service/GetFilmService.java
@@ -0,0 +1,10 @@
+package com.stylefeng.guns.rest.modular.film.service;
+
+import com.stylefeng.guns.rest.modular.film.vo.GetFilmVO;
+
+public interface GetFilmService {
+    /*影片详情*/
+    GetFilmVO selectGetFilmData();
+
+
+}
