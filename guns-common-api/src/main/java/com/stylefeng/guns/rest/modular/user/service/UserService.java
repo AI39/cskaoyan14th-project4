65892947a8c7b362ab8c0263cd998c8aca2cfd0a
baxigui <yyc05060808@gmail.com>
2019-07-18 15:59:02 +0800

    实现一二六三个api

---
+++ guns-common-api/src/main/java/com/stylefeng/guns/rest/modular/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.stylefeng.guns.rest.modular.user.service;
+
+/**
+ * @author Yuechao Yang
+ * @version 2019-07-16-22:18
+ */
+public interface UserService {
+
+   public Object selectById(int i);
+}
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/consumer/controller/ConsumerController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/consumer/controller/ConsumerController.java
@@ -1,5 +1,20 @@
 package com.stylefeng.guns.rest.modular.consumer.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.rest.modular.user.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
 public class ConsumerController {
+    @Reference
+    UserService userService;
+    @RequestMapping("test")
+    @ResponseBody
+    public String test() {
+        return userService.selectById(2).toString();
+    }
 
 }
