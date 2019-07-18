bc5d4ce3fc7f3700848b0ea8cf8e64622565cb99
liwenjing1995 <443874266@qq.com>
2019-07-18 20:46:21 +0800

    　＂ｌｉｗｅｎｊｉｎｇ＂

--- guns-common-api/src/main/java/com/stylefeng/guns/rest/modular/film/service/GetFilmService.java
+++ guns-common-api/src/main/java/com/stylefeng/guns/rest/modular/film/service/GetFilmService.java
@@ -1,17 +1,15 @@
 package com.stylefeng.guns.rest.modular.film.service;
 
-import com.stylefeng.guns.rest.modular.film.vo.FilmDetailVO;
-import com.stylefeng.guns.rest.modular.film.vo.GetFilmVO;
+        import com.stylefeng.guns.rest.modular.cinema.vo.CinemaListVo;
+        import com.stylefeng.guns.rest.modular.cinema.vo.FieldListVoData;
+        import com.stylefeng.guns.rest.modular.film.vo.FilmDateVO;
+        import com.stylefeng.guns.rest.modular.film.vo.FilmDetailVO;
+        import com.stylefeng.guns.rest.modular.film.vo.GetFilmVO;
+
+        import java.util.List;
 
 public interface GetFilmService {
     /*影片详情*/
-    GetFilmVO queryGetFilmVOById(int filmId);
-
-    GetFilmVO queryGetFilmVOByName(String filmName);
-
-    boolean selectGetFilmVOExist(int filmId);
-
-    boolean selectGetFilmVOByName(String filmName);
-
+    FilmDateVO selectGetFilmDataListByIdId(Integer showType, Integer sortId, Integer catId, Integer sourceId, Integer yearId, Integer pageSize, Integer nowPage);
 
 }
