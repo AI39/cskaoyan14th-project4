88fb22cfe61de92ff3251c8635248b9af4c12786
Xiongfei Mo <xiongfei_mo@163.com>
2019-07-20 18:00:43 +0800

    部分优化

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/controller/GetFilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/controller/GetFilmController.java
@@ -2,8 +2,6 @@ package com.stylefeng.guns.rest.modular.film.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.rest.modular.film.service.GetFilmService;
-import com.stylefeng.guns.rest.modular.film.vo.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +16,32 @@ public class GetFilmController {
     private static final String IMG_PRE = "http://img.meetingshop.cn/";
 
     @RequestMapping(value = {"getFilms"},method = {RequestMethod.GET})
-    public Object getFilmList(int showType,int sortId,int catId,int sourceId,int yearId,int nowPage,int pageSize){
-
-        return getFilmService.selectGetFilmDataListByIdId(showType,sortId,catId,sourceId,yearId,nowPage,pageSize);
+    public Object getFilmList(Integer showType,Integer sortId,Integer catId,Integer sourceId,Integer yearId,Integer nowPage,Integer pageSize, Integer offset, String kw){
+        if (showType == null) {
+            showType = 1;
+        }
+        if (sortId == null) {
+            sortId = 1;
+        }
+        if (catId == null) {
+            catId = 99;
+        }
+        if (sourceId == null) {
+            sourceId = 99;
+        }
+        if (yearId == null) {
+            yearId = 99;
+        }
+        if (nowPage == null) {
+            nowPage = 1;
+        }
+        if (pageSize == null) {
+            pageSize = 18;
+        }
+        if(offset == null) {
+            offset = 0;
+        }
+        return getFilmService.selectGetFilmDataListByIdId(showType,sortId,catId,sourceId,yearId,nowPage,pageSize,kw);
     }
 
 }
